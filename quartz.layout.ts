57566b59c3077c0f07aa52f88a4136a04b918ff2
Sean Missingham <sean@missingham.com>
2025-03-12 15:13:09 -0500

    Personalizations to Base Quartz 4
    - Include nix flake dev env
    - Minor changes to config for site personalisation
    - Update github actions for my use

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,11 +8,21 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/smissingham",
+      LinkedIn: "https://www.linkedin.com/in/sean-missingham/",
+      Email: "mailto:sean+web@missingham.com",
     },
   }),
 }
+
+const explorer = Component.Explorer({
+  title: "Explore",
+  useSavedState: true,
+  filterFn: (node) => !node.displayName.startsWith("_"),
+  mapFn: (node) => {
+    return (node.displayName = node.displayName.replace(/-/g, " "))
+  },
+})
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
@@ -34,7 +44,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    explorer,
   ],
   right: [
     Component.Graph(),
@@ -58,7 +68,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    explorer,
   ],
   right: [],
 }
